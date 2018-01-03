b671ba199413733ee2845630db5114ef92ad65cc
AndriySh <andriy.shtoyko@kindgeek.com>
2018-01-03 15:28:27 +0200

    testing flow (merge fixes)

--- dev/js/components/common/index.js
+++ dev/js/components/common/index.js
@@ -1,30 +1,17 @@
-export CheckBox from './CheckBox/CheckBox';
-export Header from './Header/Header';
-export Input from './Input/Input';
-export MainNavigation from './MainNavigation/MainNavigation';
-export Modal from './Modal/Modal';
-export RadioButton from './RadioButton/RadioButton';
-export Range from './Range/Range';
-export Select from './Select/Select';
-export Switch from './Switch/Switch';
-import ChooseSequence          from './ChooseSequenceModal/chooseSequence';
-import AssetsList              from './AssetsList/AssetsList';
-import DiagnosisRulesComponent from './DiagnosisRules/diagnosisRules';
-import Input                   from './Input/Input';
-import AsyncAreaSelect         from './AsyncAreaSelect/AsyncAreaSelect';
-import UniqueKey               from './UniqueKey/UniqueKey';
-import BlockDivider            from './BlockDivider/BlockDivider';
-import ContentPreLoader        from './PreLoaders/ContentPreLoader'
-import AssetsModal             from './AssetsModal/AssetsModal';
-
-export {
-  ChooseSequence,
-  AssetsList,
-  DiagnosisRulesComponent,
-  Input,
-  AsyncAreaSelect,
-  UniqueKey,
-  BlockDivider,
-  ContentPreLoader,
-  AssetsModal
-};
+export CheckBox                from './CheckBox/CheckBox';
+export Header                  from './Header/Header';
+export Input                   from './Input/Input';
+export MainNavigation          from './MainNavigation/MainNavigation';
+export Modal                   from './Modal/Modal';
+export RadioButton             from './RadioButton/RadioButton';
+export Range                   from './Range/Range';
+export Select                  from './Select/Select';
+export Switch                  from './Switch/Switch';
+export ChooseSequence          from './ChooseSequenceModal/chooseSequence';
+export AssetsList              from './AssetsList/AssetsList';
+export DiagnosisRulesComponent from './DiagnosisRules/diagnosisRules';
+export AsyncAreaSelect         from './AsyncAreaSelect/AsyncAreaSelect';
+export UniqueKey               from './UniqueKey/UniqueKey';
+export BlockDivider            from './BlockDivider/BlockDivider';
+export ContentPreLoader        from './PreLoaders/ContentPreLoader'
+export AssetsModal             from './AssetsModal/AssetsModal';
