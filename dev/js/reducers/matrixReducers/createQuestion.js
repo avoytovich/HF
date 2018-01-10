ae2179c1272357f80c656c113141ad2bc26d1f87
Ruslan <ruslan@jstech-inc.com>
2018-01-10 13:34:43 +0200

    RULES: hot-fix

--- dev/js/reducers/matrixReducers/createQuestion.js
+++ dev/js/reducers/matrixReducers/createQuestion.js
@@ -125,7 +125,7 @@ const setFullQuestion = (state, action) => {
       sequence: step,
       questionKey: key,
       answerType: _type,
-      rules: Array.isArray(rule) ? rule : [ rule ],
+      rules: rule ? rule.and : [],
       [_type]: parseAnswers(answer),
       diagnostic_assets: test_file ||  [],
       packageLevels: packageLevels || []
@@ -139,7 +139,7 @@ const setFullQuestionForCondition = (state, action) => {
     areaIds: configArea(areas),
     questionTitle: title,
     questionKey: key,
-    rules: Array.isArray(rule) ? rule : [ rule ],
+    rules: rule ? rule.and : [],
   };
 
   const res = body.package ?
@@ -189,7 +189,7 @@ const parseAnswers= (answer) => {
     };
   }
   else if (answer.subtype === 'range') {
-    const { max, min } = answer;
+    const { values: {max, min} } = answer;
     return {
       from: min,
       to: max
