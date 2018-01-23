39d444ddc5aba507b577a5233d00dc6c45f61496
AndriySh <andriy.shtoyko@kindgeek.com>
2018-01-23 18:09:49 +0200

    added body model component with controls

--- dev/js/components/matrix/Matrix-Setup/bodyAreaTab/BodyModel/BodyModel.js
+++ dev/js/components/matrix/Matrix-Setup/bodyAreaTab/BodyModel/BodyModel.js
@@ -1,20 +1,132 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
+import { Map, TileLayer, Circle, FeatureGroup } from 'react-leaflet';
+import L from 'leaflet';
+import { EditControl } from 'react-leaflet-draw';
 
-class BodyModel extends Component {
-  render(){
-    return(
-      <div>
+// work around broken icons when using webpack, see https://github.com/PaulLeCam/react-leaflet/issues/255
 
-      </div>
-    )
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-icon.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/marker-shadow.png',
+});
+
+//
+
+let polyline;
+
+export default class BodyModel extends Component {
+
+  // see http://leaflet.github.io/Leaflet.draw/docs/leaflet-draw-latest.html#l-draw-event for leaflet-draw events doc
+
+  _onEdited = (e) => {
+
+    let numEdited = 0;
+    e.layers.eachLayer( (layer) => {
+      numEdited += 1;
+    })
+    console.log(`_onEdited: edited ${numEdited} layers`, e);
+
+    this._onChange();
+  }
+
+  _onCreated = (e) => {
+    let type = e.layerType;
+    let layer = e.layer;
+    if (type === 'marker') {
+      // Do marker specific actions
+      console.log("_onCreated: marker created", e);
+    }
+    else {
+      console.log("_onCreated: something else created:", type, e);
+    }
+    // Do whatever else you need to. (save to db; etc)
+
+    this._onChange();
+  }
+
+  _onDeleted = (e) => {
+
+    let numDeleted = 0;
+    e.layers.eachLayer( (layer) => {
+      numDeleted += 1;
+    })
+    console.log(`onDeleted: removed ${numDeleted} layers`, e);
+
+    this._onChange();
+  }
+
+  _onMounted = (drawControl) => {
+    console.log('_onMounted', drawControl);
+  }
+
+  _onEditStart = (e) => {
+    console.log('_onEditStart', e);
+  }
+
+  _onEditStop = (e) => {
+    console.log('_onEditStop', e);
+  }
+
+  _onDeleteStart = (e) => {
+    console.log('_onDeleteStart', e);
+  }
+
+  _onDeleteStop = (e) => {
+    console.log('_onDeleteStop', e);
+  }
+
+  _editableFG = null
+
+  _onFeatureGroupReady = (reactFGref) => {
+
+    // populate the leaflet FeatureGroup with the geoJson layers
+
+
+    // store the ref for future access to content
+
+    this._editableFG = reactFGref;
+  }
+
+  _onChange = () => {
+
+    // this._editableFG contains the edited geometry, which can be manipulated through the leaflet API
+
+    const { onChange } = this.props;
+
+    if (!this._editableFG || !onChange) {
+      return;
+    }
+
+    const geojsonData = this._editableFG.leafletElement.toGeoJSON();
+    onChange(geojsonData);
+  }
+
+  render() {
+    return (
+      <Map style={{ height: '500px' }} center={[37.8189, -122.4786]} zoom={13} zoomControl={false}>
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://www.w3schools.com/bootstrap/paris.jpg"
+        />
+        <FeatureGroup ref={ (reactFGref) => {this._onFeatureGroupReady(reactFGref);} }>
+          <EditControl
+            position='topright'
+            onEdited={this._onEdited}
+            onCreated={this._onCreated}
+            onDeleted={this._onDeleted}
+            onMounted={this._onMounted}
+            onEditStart={this._onEditStart}
+            onEditStop={this._onEditStop}
+            onDeleteStart={this._onDeleteStart}
+            onDeleteStop={this._onDeleteStop}
+            draw={{
+              rectangle: false
+            }}
+          />
+        </FeatureGroup>
+      </Map>
+    );
   }
 }
-
-BodyModel.propTypes = {};
-
-BodyModel.defaultProps = {};
-
-export default BodyModel;
