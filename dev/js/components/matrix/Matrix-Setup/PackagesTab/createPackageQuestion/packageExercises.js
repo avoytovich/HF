90db5d0c932dfbd8b00d611defc3af9b75a855a4
Ruslan <ruslan@jstech-inc.com>
2018-02-02 22:21:04 +0200

    HEAL-Exercise-Percent: update

--- dev/js/components/matrix/Matrix-Setup/PackagesTab/createPackageQuestion/packageExercises.js
+++ dev/js/components/matrix/Matrix-Setup/PackagesTab/createPackageQuestion/packageExercises.js
@@ -12,9 +12,11 @@ import { connect }                from 'react-redux';
 import { bindActionCreators }     from 'redux';
 import Grid                       from 'material-ui/Grid';
 import { get }                      from 'lodash';
+import TextField                  from 'material-ui/TextField';
+
 
 class PackageExercises extends Component  {
-    state = { list: [] };
+    state = { list: [], error: false };
     componentDidMount() {
       this.props.exercises.length &&
       getPackageLevel('exercises', 'getExercises', this.props.exercises, this.props.level)
@@ -40,13 +42,31 @@ class PackageExercises extends Component  {
       updateCrateQuestionFields(filtered, `packageLevels[${this.props.level}].exercise_ids`)
     };
 
+
+    onProbabilityChange = (event, index) => {
+      const value = event.target.value;
+      const list = this.state.list.map((item, i) => {
+        if (index === i) {
+          return Object.assign({}, item, {probability: value});
+        }
+        return item;
+      });
+      const tooMuch = list.reduce((result, item) => {
+        if (item) {
+          return result + +item.probability;
+        }
+        return item;
+      }, 0);
+      this.setState({list, error: tooMuch > 100});
+      console.log('error', this.state.error)
+    };
+
     render() {
       return (
         <Grid item xs={12} className="package-level-exercises-list">
           {this.state.list.map((item, index) => {
-           const { id, title, created_at } = item;
+           const { id, title, created_at, probability } = item;
            const created = moment.unix(created_at).format(TIME_FORMAT_DOTS);
-
            return <div key={index} className="package-level-exercises-item">
 
              <div className="exercises-information">
@@ -56,9 +76,20 @@ class PackageExercises extends Component  {
                </Typography>
 
                <Typography type="body2">
-                 Created { created }
+                 Created { created } \\ error: {this.state.error}
                </Typography>
 
+             </div>
+
+             <div>
+               <TextField
+                 id={'probability-'+ index}
+                 label="Probability"
+                 value={ probability || ''}
+                 onChange={value => this.onProbabilityChange(value, index)}
+                 margin="normal"
+                 fullWidth={true}
+               />
              </div>
 
              <div className="delete-icon">
