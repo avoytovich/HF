e14ef51e53c207a70efca2c295a7f135315cecd7
Marta Dropko <marta.dropko@kindgeek.com>
2018-02-09 16:49:16 +0200

    fix tmp files

--- dev/js/components/assets/AssetsDiagnosticsList/AssetsDiagnosticsList.js
+++ dev/js/components/assets/AssetsDiagnosticsList/AssetsDiagnosticsList.js
@@ -53,7 +53,7 @@ class AssetsList extends Component {
       return {
         onClick: (e) => {
           e.stopPropagation();
-          dispatchAssetsPayloadWired({ [`files[${0}]`]: { ...row, progress: 100 } });
+          dispatchAssetsPayloadWired({ [`tmp_files[${0}]`]: { ...row, progress: 100 } });
           this._toggleEditModal()
         }
       }
