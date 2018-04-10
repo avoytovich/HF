63c61d965b58c3daa35d0ef1f977629e2e360d92
Petro Pavlenko <petro.pavlenko@kindgeek.com>
2018-04-10 15:26:33 +0300

    add load appInfo

--- dev/js/reducers/appScreenInfoReducer/appScreenInfoReducer.js
+++ dev/js/reducers/appScreenInfoReducer/appScreenInfoReducer.js
@@ -4,20 +4,62 @@ import { APP_INFO } from '../../actions';
 const initialState = {
   actionType                : APP_INFO,
   lang                      : 'en',
-  home_screen               : { en: '', swe: ''},
-  start_session_screen      : { en: '', swe: ''},
-  details_session_screen    : { en: '', swe: ''},
-  self_diagnosis_screen     : { en: '', swe: ''},
-  health_history_screen     : { en: '', swe: ''},
-  pain_profile_screen       : { en: '', swe: ''},
-  diagnostic_test_screen    : { en: '', swe: ''},
-  progress_screen           : { en: '', swe: ''},
-  prognosis_screen          : { en: '', swe: ''},
-  schedule_screen           : { en: '', swe: ''},
-  reminders_screen          : { en: '', swe: ''},
-  favourite_exercises_screen: { en: '', swe: ''},
-  activity_journal_screen   : { en: '', swe: ''},
-  profile_screen            : { en: '', swe: ''}
+  home_screen               : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  start_session_screen      : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  details_session_screen    : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  self_diagnosis_screen     : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  health_history_screen     : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  pain_profile_screen       : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  diagnostic_test_screen    : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  progress_screen           : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  prognosis_screen          : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  schedule_screen           : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  reminders_screen          : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  favourite_exercises_screen: {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  activity_journal_screen   : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
+  profile_screen            : {
+    information: { en: '', swe: ''},
+    instruction: { en: '', swe: ''}
+  },
 };
 
 const addInfo = (state, action) => {
