da9c64ec782204a99f78fde1204f93e98c21ef6c
Myshchyshyn Oleksandr <vbobiby.my@gmail.com>
2018-05-23 16:59:39 +0300

    update user table & fix HEAL-1070

--- dev/js/utils/constants/apiKeys.js
+++ dev/js/utils/constants/apiKeys.js
@@ -2,4 +2,4 @@ const environment = process.env.NODE_CUSTOM_MODE;
 
 export const stripeKey = (environment === 'development') ?
   'pk_test_nd2AO9CvcrB17TXhe5kwjd8I' : 'pk_live_KuawtiI0tEN6URZyrVXrFDKF';
-export const socketUrl = "http://18.195.77.253:3000";
+export const socketUrl = "http://18.195.167.42:3000";
